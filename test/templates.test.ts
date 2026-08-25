037d5bc23639ddbea89ed86ead37e30c91eaefdb
Emmanuel Gautier <git@emmanuelgautier.fr>
2026-08-25 22:59:30 +0200

    feat: migrate nacho templates to cimd json files

--- test/templates.test.ts
+++ test/templates.test.ts
@@ -5,7 +5,7 @@ import { makeTestEnv } from './testKv'
 describe('static CIMD templates', () => {
   it('does not route template requests through Hono', async () => {
     const env = makeTestEnv()
-    const res = await app.request('/react-client.json', {}, env)
+    const res = await app.request('/react-spa-client.json', {}, env)
     expect(res.status).toBe(404)
   })
 })
